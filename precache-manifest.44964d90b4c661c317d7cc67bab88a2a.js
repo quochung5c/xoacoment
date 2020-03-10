ef52cb8ebf9367b1ca98487fd13920b10139efde
Maximuss Hùng <vidostartup@gmail.com>
2020-03-10 17:18:16 +0700

    Updates

---
+++ precache-manifest.44964d90b4c661c317d7cc67bab88a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060ff0bcb02f1489f2468bc57e349792",
+    "url": "/xoacoment/index.html"
+  },
+  {
+    "revision": "ddd7d15311ed5e23fff6",
+    "url": "/xoacoment/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "8d5e3690140a47d4bcbe",
+    "url": "/xoacoment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ddd7d15311ed5e23fff6",
+    "url": "/xoacoment/static/js/2.66c34e8e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/xoacoment/static/js/2.66c34e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5e3690140a47d4bcbe",
+    "url": "/xoacoment/static/js/main.63966b8d.chunk.js"
+  },
+  {
+    "revision": "bdb394d15b3c8e510d15",
+    "url": "/xoacoment/static/js/runtime-main.a92acbe3.js"
+  }
+]);
